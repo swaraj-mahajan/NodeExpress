d93dd49d792c599c982cb3acf392db9075716374
Swaraj Mahajan <swaraj.mahajanl@ggktech.com>
2018-12-26 14:40:07 +0530

    updated package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -33,6 +33,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('App started on port 3000')
+app.listen(port, () => {
+  console.log(`App started on port ${port}`)
 });
